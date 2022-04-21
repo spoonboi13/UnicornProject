86eaf36bd4da1044f2cb060abfe939cbda1e2b65
zoe luptowski <zoeluptowski@my.unt.edu>
2022-04-20 19:03:09 -0500

    changed userpoolID/clientID and region

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_jyXyo4zUy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4j38bh2j18fosc11s43k6p23gc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
